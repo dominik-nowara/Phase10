44ab19f970179154d82d12f514b0a306649e8d15
Dominik Nowara <132879273+dominik-nowara@users.noreply.github.com>
2024-04-18 23:25:24 +0200

    added: card random exchange method
    Signed-off-by: Dominik Nowara <132879273+dominik-nowara@users.noreply.github.com>

--- src/main/scala/phase10/models/CardHand.scala
+++ src/main/scala/phase10/models/CardHand.scala
@@ -11,7 +11,12 @@ case class CardHand(cards: List[Card] = randomStack()) {
   override def toString: String = cards.mkString(", ") + s" (${cards.length})"
 
   def removeCard(index: Int): CardHand = {
-    val newCards = cards.slice(0, index)++ cards.slice(index + 1, cards.length)
+    val newCards = cards.slice(0, index) ++ cards.slice(index + 1, cards.length)
+    copy(newCards)
+  }
+
+  def changeCard(index: Int): CardHand = {
+    val newCards = cards.slice(0, index) ++ cards.slice(index + 1, cards.length) :+ randomCard()
     copy(newCards)
   }
 }
