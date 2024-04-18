8dce8fe7b9f30464fe7390d72d165fbe2b16f8f8
Dominik Nowara <132879273+dominik-nowara@users.noreply.github.com>
2024-04-18 23:19:51 +0200

    refactor: Player model with CardHand
    Signed-off-by: Dominik Nowara <132879273+dominik-nowara@users.noreply.github.com>

--- src/main/scala/phase10/models/Player.scala
+++ src/main/scala/phase10/models/Player.scala
@@ -1,8 +1,5 @@
 package phase10.models
 
-case class Player(val name: String, val cards: List[Card]) {
-  //Print list of cards into console
-  override def toString: String = {
-    cards.mkString(", ")
-  }
+case class Player(val name: String, val cards: CardHand) {
+  override def toString: String = name + ": " + cards.toString
 }
