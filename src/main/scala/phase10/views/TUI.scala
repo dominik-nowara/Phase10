c31502a1e15cc67c9dcfcb80889ba66b67b3a065
Dominik Nowara <132879273+dominik-nowara@users.noreply.github.com>
2024-04-18 23:25:59 +0200

    added: Printing of player
    Signed-off-by: Dominik Nowara <132879273+dominik-nowara@users.noreply.github.com>

--- src/main/scala/phase10/views/TUI.scala
+++ src/main/scala/phase10/views/TUI.scala
@@ -1,11 +1,20 @@
 package phase10.views
 
-import scala.io.AnsiColor._
+import phase10.models.*
+
+import scala.io.AnsiColor.*
+import scala.io.StdIn.readLine
 
 object TUI {
   def Initialize(): Unit = {
     println(s"${GREEN}${BOLD}Welcome to Phase 10!${RESET}")
     println(s"${BLUE}Press 'q' to quit the game")
-    println(s"Press 'h' to show the help menu${RESET}")
+    println(s"Press 'h' to show the help menu${RESET}\n")
+    println("Please enter your name:")
+
+    val playerName = readLine()
+
+    val player = Player(playerName, CardHand())
+    println(player)
   }
 }
