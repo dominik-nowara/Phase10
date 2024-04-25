69b3b78aea16de0bb3ac3898f289ac4f3b74b352
Dominik Nowara <132879273+dominik-nowara@users.noreply.github.com>
2024-04-25 18:59:00 +0200

    update: changed main to new mvc approach

--- src/main/scala/phase10/Main.scala
+++ src/main/scala/phase10/Main.scala
@@ -1,18 +1,16 @@
 package phase10
 
+import phase10.controller.GameController
+
 import scala.util.Random
 import phase10.models.*
 import phase10.views.TUI
 
 object Main {
   def main(args: Array[String]): Unit = {
-    TUI.Initialize()
-
-    /*
-    val phaseList = List.fill(10)(randomPhases())
-    val list = List.fill(10)(randomCard())
-    
-    println(phaseList.toString())
-    println(list.toString())*/
+    val round = initRound()
+    val controller = GameController(round)
+    val tui = TUI(controller)
+    tui.initialize()
   }
 }
