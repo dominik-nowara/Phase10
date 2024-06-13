0526b190f5d6f725bcdbb9d092a167bcff3c388b
SirKurd <132879273+dominik-nowara@users.noreply.github.com>
2024-06-13 23:55:46 +0200

    removed: Buggy TUI test

--- src/test/scala/views/TuiSpec.scala
+++ src/test/scala/views/TuiSpec.scala
@@ -21,32 +21,6 @@ class TuiSpec extends AnyWordSpec {
       }
 
     }
-    "initialize screen should look like" in {
-      val inStream = new ByteArrayInputStream("2".getBytes())
-      val outStream = new ByteArrayOutputStream()
-      val outCapture = ByteArrayOutputStream()
-
-      tui.continue = false
-
-      Console.withOut(outCapture) {
-        Console.withIn(inStream) {
-          tui.initialize()
-        }
-      }
-      outCapture.toString should be(s"${BLACK_B}${BOLD}" +
-        s"${BLACK_B}  ${RED}______ ${BLUE} _                      ${YELLOW}__  ${RED}___    ${RESET}\n" +
-        s"${BLACK_B}  ${RED}|  __ \\${BLUE}| |                    ${YELLOW}/_ |${RED}/ _ \\   ${RESET}\n" +
-        s"${BLACK_B}  ${RED}| |__) ${BLUE}| |__   ${YELLOW}__ _ ${GREEN}___  ${BLUE}___   ${YELLOW}| | ${RED}| | |  ${RESET}\n" +
-        s"${BLACK_B}  ${RED}|  ___/${BLUE}| '_ \\ ${YELLOW}/ _` ${GREEN}/ __|${BLUE}/ _ \\  ${YELLOW}| |${RED} | | |  ${RESET}\n" +
-        s"${BLACK_B}  ${RED}| |    ${BLUE}| | | |${YELLOW} (_| ${GREEN}\\__ \\${BLUE}  __/  ${YELLOW}| |${RED} |_| |  ${RESET}\n" +
-        s"${BLACK_B}  ${RED}|_|    ${BLUE}|_| |_|${YELLOW}\\__,_${GREEN}|___/${BLUE}\\___|  ${YELLOW}|_|${RED}\\___/   ${RESET}\n" +
-        s"${BLACK_B}                                            ${RESET}\n\n" +
-        s"${BLUE}Press 'q' to quit the game\n" +
-        s"Press 'h' to show the help menu and game instructions${RESET}\n\n" +
-        s"${BOLD}${BLACK_B} Please enter the amount of players: ${RESET}\n" +
-        "\n" * 50 +
-        s"${GREEN}${BLACK_B} Is the first player ready? (Press 'y' if you're ready) ${RESET}\n")
-    }
     "print spaces should look like" in {
       val outCapture = ByteArrayOutputStream()
       Console.withOut(outCapture) {
